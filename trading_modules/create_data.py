8f7d91e22dc96877752174e88801d699bc9f540d
fotino21 <ataturhan21@gmail.com>
2023-04-27 18:27:04 +0300

    feature: new functions added to create_data module from alpha trading bot

--- trading_modules/create_data.py
+++ trading_modules/create_data.py
@@ -7,6 +7,40 @@ import sklearn.linear_model
 import talib
 import tensorflow as tf
 from sklearn.linear_model import LinearRegression
+import datetime as dt
+import json
+import random
+import pandas_datareader as pdr
+import requests
+import yfinance as yf
+from plotly.subplots import make_subplots
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import (
+    RFE,
+    SelectFromModel,
+    SelectKBest,
+    f_classif,
+)
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from ta import add_all_ta_features
+
+fred_codes = {
+    "FED 2Y Interest Rate": "DGS2",
+    "FED 10Y Interest Rate": "DGS10",
+    "30-Year Fixed Rate Mortgage Average in the United States": "MORTGAGE30US",
+    "Unemployment Rate": "UNRATE",
+    "Real Gross Domestic Product": "GDPC1",
+    "Gross Domestic Product": "GDP",
+    "10-Year Breakeven Inflation Rate": "T10YIE",
+    "Median Sales Price of Houses Sold for the United States": "MSPUS",
+    "Personal Saving Rate": "PSAVERT",
+    "Deposits, All Commercial Banks": "DPSACBW027SBOG",
+    "S&P 500": "SP500",
+    "Federal Debt: Total Public Debt as Percent of Gross Domestic Product": "GFDEGDQ188S",
+    "Crude Oil Prices: West Texas Intermediate (WTI) - Cushing, Oklahoma": "DCOILWTICO",
+    "Consumer Loans: Credit Cards and Other Revolving Plans, All Commercial Banks": "CCLACBW027SBOG",
+    "Consumer Price Index for All Urban Consumers: All Items Less Food and Energy in U.S. City Average": "CPILFESL",
+}
 
 
 def HMA(df: pd.DataFrame, timeperiod: int = 14) -> float:
@@ -84,19 +118,18 @@ def trendNormalizePrices(prices: pd.DataFrame) -> None:
     return df
 
 
-def create_labels(prices: pd.DataFrame) -> None:
-    df = prices.copy()
+def create_labels(ohlcv: pd.DataFrame) -> None:
+    df = ohlcv.copy()
     df["Label"] = [0] * df.shape[0]
     for i in range(df.shape[0] - 10):
         s = set(df["Close"].iloc[i : i + 11])
-        minPrice = sorted(s)[0]
-        maxPrice = sorted(s)[-1]
+        minPrice, maxPrice = sorted(s)[0], sorted(s)[-1]
         for j in range(i, i + 11):
             if df["Close"].iloc[j] == minPrice and (j - i) == 5:
                 df.iloc[j, df.columns.get_loc("Label")] = 1
             elif df["Close"].iloc[j] == maxPrice and (j - i) == 5:
                 df.iloc[j, df.columns.get_loc("Label")] = 2
-    return df.iloc[6:-6]
+    return df
 
 
 def reverse_one_hot(predictions: np.array) -> np.array:
@@ -135,6 +168,54 @@ def show_label_distribution(df: pd.DataFrame):
     )
     fig = px.bar(label_and_counts, x="Label Names", y="Label Counts")
     fig.show()
+
+
+def show_multiple_prices(
+    data: pd.DataFrame, ticker: str, show_which_price: str
+) -> None:
+    colors = ["red", "green", "blue", "orange", "black", "magenta", "cyan"]
+    fig = make_subplots(
+        rows=2,
+        cols=1,
+        shared_xaxes=False,
+        vertical_spacing=0.1,
+        subplot_titles=(f"Chart", f"Volume"),
+        row_width=[1, 5],
+    )
+    if "Candlestick" in show_which_price:
+        show_which_price.remove("Candlestick")
+        fig.add_trace(
+            go.Candlestick(
+                x=data.index,
+                open=data["Open"],
+                high=data["High"],
+                low=data["Low"],
+                close=data["Close"],
+                name="OHLC",
+            ),
+            row=1,
+            col=1,
+        )
+    for column in show_which_price:
+        fig.add_trace(
+            go.Scatter(
+                x=data.index,
+                y=data[column],
+                mode="lines",
+                line=dict(color=random.choice(colors)),
+                name=f"{column}",
+            ),
+            row=1,
+            col=1,
+        )
+    fig.add_trace(go.Bar(x=data.index, y=data["Volume"], name="Volume"), row=2, col=1)
+    fig.update(layout_xaxis_rangeslider_visible=False)
+    fig.update_layout(
+        autosize=True,
+        width=950,
+        height=950,
+    )
+    return fig
 
 
 def show_prices(ticker: str, df: pd.DataFrame, desc: str = ""):
@@ -185,6 +266,140 @@ def show_price_and_labels(ticker: str, df: pd.DataFrame, desc: str = ""):
     msg = desc or f"Close Price with Labels of '{ticker}'"
     fig.update_layout(title=msg, title_x=0.5)
     fig.show()
+
+
+def signal_smoothing(
+    df: pd.DataFrame, smoothing_method: str = "None", parameters: dict = None
+):
+    data = df.copy(deep=True)
+    if smoothing_method == "None":
+        return data
+    elif smoothing_method == "Moving Average":
+        data["Open"] = data["Open"].rolling(parameters["window"]).mean()
+        data["High"] = data["High"].rolling(parameters["window"]).mean()
+        data["Low"] = data["Low"].rolling(parameters["window"]).mean()
+        data["Close"] = data["Close"].rolling(parameters["window"]).mean()
+        # data.dropna(inplace=True)
+    elif smoothing_method == "Heikin-Ashi":
+        data = data.assign(HeikinAshi_Open=np.zeros((data.shape[0])))
+        data = data.assign(HeikinAshi_High=np.zeros((data.shape[0])))
+        data = data.assign(HeikinAshi_Low=np.zeros((data.shape[0])))
+        data = data.assign(HeikinAshi_Close=np.zeros((data.shape[0])))
+        data.iloc[0, data.columns.get_loc("HeikinAshi_Open")] = (
+            data["Open"].iloc[0] + data["Close"].iloc[0]
+        ) / 2
+        for i in range(data.shape[0]):
+            if i != 0:
+                data.iloc[i, data.columns.get_loc("HeikinAshi_Open")] = (
+                    data["HeikinAshi_Open"].iloc[i - 1]
+                    + data["HeikinAshi_Close"].iloc[i - 1]
+                ) / 2
+            data.iloc[i, data.columns.get_loc("HeikinAshi_Close")] = (
+                data["Open"].iloc[i]
+                + data["High"].iloc[i]
+                + data["Low"].iloc[i]
+                + data["Close"].iloc[i]
+            ) / 4
+            data.iloc[i, data.columns.get_loc("HeikinAshi_High")] = max(
+                [
+                    data["High"].iloc[i],
+                    data["HeikinAshi_Open"].iloc[i],
+                    data["HeikinAshi_Close"].iloc[i],
+                ]
+            )
+            data.iloc[i, data.columns.get_loc("HeikinAshi_Low")] = min(
+                [
+                    data["Low"].iloc[i],
+                    data["HeikinAshi_Open"].iloc[i],
+                    data["HeikinAshi_Close"].iloc[i],
+                ]
+            )
+        data.drop(index=data.index[:30], axis=0, inplace=True)
+        data["Open"] = data["HeikinAshi_Open"]
+        data["High"] = data["HeikinAshi_High"]
+        data["Low"] = data["HeikinAshi_Low"]
+        data["Close"] = data["HeikinAshi_Close"]
+        data.drop(
+            [
+                "HeikinAshi_Open",
+                "HeikinAshi_High",
+                "HeikinAshi_Low",
+                "HeikinAshi_Close",
+            ],
+            axis=1,
+            inplace=True,
+        )
+    elif smoothing_method == "Trend Normalization":
+        data["rowNumber"] = list(range(len(data)))
+        data["TN_Open"] = list(range(len(data)))
+        data["TN_High"] = list(range(len(data)))
+        data["TN_Low"] = list(range(len(data)))
+        data["TN_Close"] = list(range(len(data)))
+        for i in range(29, len(data)):
+            model = LinearRegression()
+            model.fit(
+                np.array(data["rowNumber"].iloc[i - 29 : i + 1]).reshape(-1, 1),
+                np.array(data["Close"].iloc[i - 29 : i + 1]),
+            )
+            prediction = model.predict(
+                np.array([data["rowNumber"].iloc[i]]).reshape(-1, 1)
+            )
+            data.iloc[i, data.columns.get_loc("TN_Open")] = (
+                data["Open"].iloc[i] - prediction
+            )
+            data.iloc[i, data.columns.get_loc("TN_High")] = (
+                data["High"].iloc[i] - prediction
+            )
+            data.iloc[i, data.columns.get_loc("TN_Low")] = (
+                data["Low"].iloc[i] - prediction
+            )
+            data.iloc[i, data.columns.get_loc("TN_Close")] = (
+                data["Close"].iloc[i] - prediction
+            )
+        data["Open"] = data["TN_Open"]
+        data["High"] = data["TN_High"]
+        data["Low"] = data["TN_Low"]
+        data["Close"] = data["TN_Close"]
+        data.drop(index=data.index[:30], axis=0, inplace=True)
+        data.loc[data.index[:30], ["Open", "High", "Low", "Close"]] = 0
+        data.drop(
+            [
+                "rowNumber",
+                "TN_Open",
+                "TN_High",
+                "TN_Low",
+                "TN_Close",
+            ],
+            axis=1,
+            inplace=True,
+        )
+    return data
+
+
+def create_fundamental_data(ohlcv: pd.DataFrame) -> pd.DataFrame:
+    start = ohlcv.index[0]
+    end = ohlcv.index[-1]
+    _, col2, _ = st.columns([1, 2, 1])
+    with col2:
+        with st.spinner("Fetching fundamental data"):
+            data = fetch_fundamental_data(ohlcv, start, end)
+            return data
+
+
+def create_ta_technical_indicators(ohlcv: pd.DataFrame) -> pd.DataFrame:
+    _, col2, _ = st.columns([1, 2, 1])
+    with col2:
+        with st.spinner("Fetching indicator data"):
+            data = add_all_ta_features(
+                ohlcv,
+                open="Open",
+                high="High",
+                low="Low",
+                close="Close",
+                volume="Volume",
+                colprefix="ta_",
+            )
+            return data
 
 
 def create_all_indicators_in_talib(df: pd.DataFrame, periods: list):
@@ -403,3 +618,85 @@ def create_2d_data(datas: list, length: int):
         [X_train_list[i], y_train_list[i], X_test_list[i], y_test_list[i]]
         for i in range(len(datas))
     ]
+
+
+def plot_confusion_matrix(cm, labels, title):
+    # cm : confusion matrix list(list)
+    # labels : name of the data list(str)
+    # title : title for the heatmap
+    data = go.Heatmap(z=cm, y=labels, x=labels)
+    annotations = []
+    for i, row in enumerate(cm):
+        annotations.extend(
+            {
+                "x": labels[i],
+                "y": labels[j],
+                "font": {"color": "white"},
+                "text": str(value),
+                "xref": "x1",
+                "yref": "y1",
+                "showarrow": False,
+            }
+            for j, value in enumerate(row)
+        )
+    layout = {
+        "title": title,
+        "xaxis": {"title": "Predicted value"},
+        "yaxis": {"title": "Real value"},
+        "annotations": annotations,
+    }
+    return go.Figure(data=data, layout=layout)
+
+
+def fetch_fred_data(start_date: str, end_date: str) -> pd.DataFrame:
+    fred_data = pdr.fred.FredReader(
+        symbols=fred_codes.values(), start=start_date, end=end_date
+    ).read()
+    fred_data.fillna(method="ffill", inplace=True)
+    fred_data["10-2 Year Yield Difference"] = fred_data["DGS10"] - fred_data["DGS2"]
+    fred_data.rename(
+        columns={v: k for k, v in fred_codes.items()},
+        inplace=True,
+    )
+    return fred_data
+
+
+def fetch_fundamental_data(data: pd.DataFrame, start_date, end_date) -> pd.DataFrame:
+    fed_data_exists = False
+    cpi_data_exists = True
+    fundamentals = [
+        "FED 2Y Interest Rate",
+        "FED 10Y Interest Rate",
+        "Yield Difference",
+    ]
+    for fundamental in fundamentals[:3]:
+        if fundamental in data.columns:
+            fed_data_exists = True
+
+    data = data.tz_localize(None)
+    data.index.names = ["Date"]
+
+    if not fed_data_exists:
+        fed_data = fetch_fred_data(start_date - pd.DateOffset(months=6), end_date)
+        data["merg_col"] = data.index.strftime("%Y-%m-%d")
+        fed_data["merg_col"] = fed_data.index.strftime("%Y-%m-%d")
+        data = (
+            data.reset_index()
+            .merge(fed_data, on="merg_col", how="left")
+            .set_index("Date")
+            .drop(columns="merg_col")
+        )
+
+    if not cpi_data_exists:
+        cpi_data = fetch_cpi_data(start_date - pd.DateOffset(months=1), end_date)
+        data.index = data.index - pd.DateOffset(months=1)
+        data["merg_col"] = data.index.strftime("%Y-%m")
+        cpi_data["merg_col"] = cpi_data.index.strftime("%Y-%m")
+        data = (
+            data.reset_index()
+            .merge(cpi_data, on="merg_col", how="left")
+            .set_index("Date")
+            .drop(columns="merg_col")
+        )
+        data.index = data.index + pd.DateOffset(months=1)
+    return data
